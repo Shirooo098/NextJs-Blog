2902c765cce8e3812310520d537f66a086623608
Shirooo098 <johndavid8803@gmail.com>
2025-07-03 00:06:41 +0800

    Responsive Nav

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -3,11 +3,11 @@ import NavLinks from "./nav-links";
 
 export default function Nav(){
     return(
-       <div className="flex justify-between py-6 px-4 sm:px-6 md:px10 lg:px-16 xl:px-24 size-auto">
+       <div className="flex flex-col sm:flex-row justify-between py-6 px-4 sm:px-6 md:px10 lg:px-16 xl:px-24 size-auto">
         <div className="flex justify-center h-12">
             <DevblogLogo variant="light"/>
         </div>
-        <div className="flex w-80">
+        <div className="flex justify-center w-full sm:w-80">
             <NavLinks/>
         </div>
        </div>
