c57bdacd18d457a0713f760ae132c045ab7655ad
Shirooo098 <johndavid8803@gmail.com>
2025-07-07 19:03:00 +0800

    Changed jfif format to png

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -20,12 +20,12 @@ export default function Categories(){
 
     return(
         <>
-            <div className="h-full py-6 px-8 sm:px-6 md:px-10 lg:px-16 xl:px-24">
+            <div className="h-full px-8 py-6 sm:px-6 md:px-10 lg:px-16 xl:px-24">
                 <div className="flex flex-col ">
                     <h1 className={`${robotoMono.className} px-6 py-2 text-2xl font-semibold md:text-4xl lg:text-5xl `}>Categories</h1>
                     <p className={`${robotoMono.className} inline-block w-full px-6 py-2 text-sm md:text-lg lg:text-xl  border-b-2 border-b-black `}>Explore more categories and venture out my journey.</p>
                 </div>
-                <div className="flex flex-wrap gap-2 w-full p-6 border-b-2 border-b-black">
+                <div className="flex flex-wrap w-full gap-2 p-6 border-b-2 border-b-black">
                     <button 
                         onClick={() => setCategory(null)}
                         className={`py-1.5 md:py-2 px-6 md:px-10 rounded-full border-2 transition-all
--- app/lib/placeholder.ts
+++ app/lib/placeholder.ts
@@ -8,55 +8,55 @@ export const cardsData : {
         title: 'My first Express App',
         date: 'December 23, 2024',
         category: 'Web-Development',
-        imageUrl: '/blogs/express.jfif'
+        imageUrl: '/blogs/express.png'
     },
     {
         title: 'My first Typescript App',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     },
     {
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     },
     {
         title: 'My first Typescript App',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     },
     {
         title: 'My first Typescript App',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     },
     {
         title: 'My first Typescript App',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     },
     {
         title: 'My first Typescript App',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     },
     {
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     }, 
     {
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
         date: 'June 21, 2024',
         category: 'Javascript',
-        imageUrl: '/blogs/typescript.jfif'
+        imageUrl: '/blogs/typescript.png'
     },
 ]
 
