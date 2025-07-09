09922acf54c1bbfca01e5d8f76c05e1d3eb140e7
Shirooo098 <johndavid8803@gmail.com>
2025-07-10 06:42:43 +0800

    Add CSS on sidenav

--- app/ui/admin/sidenav-links.tsx
+++ app/ui/admin/sidenav-links.tsx
@@ -1,4 +1,9 @@
+'use client'
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import { robotoMono } from '../fonts';
 
 const links = [
     {
@@ -16,6 +21,8 @@ const links = [
 ]
 
 export default function SideNavLinks(){
+    const pathname = usePathname();
+
     return(
         <>
             {links.map((link => {
@@ -23,8 +30,15 @@ export default function SideNavLinks(){
                     <Link
                         key={link.name}
                         href={link.href}
+                        className={clsx(
+                            `flex h-[48px] grow items-center justify-center gap-2
+                             rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 
+                             hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3` , {
+                                'bg-sky-200 text-blue-800' : pathname === link.href
+                            }
+                        )} 
                     >
-                        <p>{link.name}</p>
+                        <p className={`${robotoMono.className} text-xl`}>{link.name}</p>
                     </Link>
                 )
             }))}
