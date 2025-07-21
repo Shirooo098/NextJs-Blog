8c94477f69a701617a869538a9e3168bc3eaaae8
Shirooo098 <johndavid8803@gmail.com>
2025-07-21 16:55:11 +0800

    Refactor Queries

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,3 +1,5 @@
+import { useQuery } from "@tanstack/react-query";
+
 export type State = {
     errors?: {
         title?: string[],
@@ -8,13 +10,28 @@ export type State = {
     message?: string | null
 }
 
-export type Blogs = {
-    id: string;
+type Blogs = {
+    id: number;
     title: string;
     category: string;
     date: string;
     description: string;
     imageUrl: string;
 }
+
+const fetchRecentBlogs = async (limit = 6 ): Promise<Array<Blogs>> => {
+    const response = await fetch('/api/blogs');
+    const data = await response.json();
+    return data.filter((x: Blogs) => x.id <= limit)
+}
+
+const useRecentBlogs = (limit : number ) => {
+    return useQuery({
+        queryKey: ['blogs', limit],
+        queryFn: () => fetchRecentBlogs(limit)
+    })
+}
+
+export { fetchRecentBlogs, useRecentBlogs};
 
 export type CategoryType = 'Web-Development' | 'Javascript' | 'Productivity' | 'Projects';
