6f325a4fac234f353c2bd8ed2356c1f692417a7c
Shirooo098 <johndavid8803@gmail.com>
2025-07-24 00:33:46 +0800

    Add Promise to view-blog page.tsx

--- app/(main)/blogs/[id]/view-blog/page.tsx
+++ app/(main)/blogs/[id]/view-blog/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
     title: 'View Blog',
 }
 
-export default async function Page({ params }: { params: { id: string}}){
+export default async function Page({ params }: { params: Promise<{ id: string}>}){
     const { id } = await params
     return <BlogPage id={id}/>
 }
