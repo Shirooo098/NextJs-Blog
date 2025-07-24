a28f9058b46bd77fb26e6f09b1cf4464149ffda0
Shirooo098 <johndavid8803@gmail.com>
2025-07-24 20:42:25 +0800

    Initialize Blogpage with ID  CSS

--- app/ui/blogs/blog.tsx
+++ app/ui/blogs/blog.tsx
@@ -2,6 +2,7 @@
 
 import { useBlogById } from "@/app/lib/data";
 import Image from "next/image";
+import { manRope } from "../fonts";
 
 export default function BlogPage({id}: {id: string}){
     const { data, isPending, isError, error } = useBlogById(id);
@@ -11,20 +12,25 @@ export default function BlogPage({id}: {id: string}){
     if(isError) return <div>Error, {error.message}</div>
 
     return(
-        <div>
-            <p>{data.id}</p>
-            <p>{data.title}</p>
-            <p>{data.date}</p>
-            <p>{data.category}</p>
-            <Image
-            loading='lazy'
-                placeholder='empty'
-                priority={false}
-                src={data.imageUrl}
-                alt={data.title}
-                height={500}
-                width={500}
-            />
-        </div>
+        <>
+            <div>
+                <div className="mb-8 w-full h-[80vh] text-center">
+                    <div className="w-full h-[70vh] flex flex-col z-10 items-center justify-center absolute">
+                        <p>{data.category}</p>
+                        <p className={`${manRope.className} capitalize text-white text-2xl font-semibold lg:text-5xl md:text-3xl`}>{data.title}</p>
+                    </div>
+                    <Image
+                        loading='lazy'
+                        placeholder='empty'
+                        priority={false}
+                        src={data.imageUrl}
+                        alt={data.title}
+                        height={500}
+                        width={500}
+                        className="z-0 aspect-square size-full object-cover object-center"
+                    />
+                </div>                   
+            </div>
+        </>
     )
 }
