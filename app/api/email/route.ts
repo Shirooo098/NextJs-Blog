4394117e622aa907d0fde599e8064502debe5162
Shirooo098 <johndavid8803@gmail.com>
2025-08-02 02:34:03 +0800

    Revert to SMTP

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,76 +1,47 @@
 import { NextRequest, NextResponse } from "next/server";
-// import nodemailer from 'nodemailer';
-// import Mail from "nodemailer/lib/mailer";
-import sgMail from "@sendgrid/mail";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
 
 export async function POST(req: NextRequest){
     const { name, email, message } = await req.json();
-    
-    try {
-        sgMail.setApiKey(process.env.SG_API_KEY!);
-        const msg = {
-            to: email, 
-            from: name, 
-            subject: 'DevBlog Contact Form',
-            text: `Hello! My name is ${name} and I want to discuss a potential project.\nYou can email me at ${email}.\nHere are some details about my project: ${message}` 
+
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, 
+        auth: {
+            user: process.env.MY_TIPGMAIL,
+            pass: process.env.GMAIL_PASSWORD
         }
+    })
 
-        sgMail.send(msg);
-        console.log(msg)
-
-        return NextResponse.json(
-            { message: "Email sent successfully" },
-            { status: 200 }
-        )
-    } catch (error) {
-        console.error("Error sending email:", error)
-        return NextResponse.json(
-            { message: "Failed to send email"},
-            { status: 500 }
-        )
+    const mailOptions: Mail.Options = {
+        from: email,
+        to: process.env.MY_TIPGMAIL,
+        subject: `Message from ${name}`,
+        text: `Hello! My name is ${name} and I want to discuss a potential project.\nYou can email me at ${email}.\nHere are some details about my project: ${message}`
     }
 
+    console.log(mailOptions)
 
-    // const transport = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     host: 'smtp.gmail.com',
-    //     port: 587,
-    //     secure: false, 
-    //     auth: {
-    //         user: process.env.MY_TIPGMAIL,
-    //         pass: process.env.GMAIL_PASSWORD
-    //     }
-    // })
+    const sendMailPromise = () => {
+        new Promise<string>((resolve, reject) => {
+            transport.sendMail(mailOptions, (err) => {
+                if(!err){
+                    resolve('Email Sent!');
+                }else{
+                    reject(err.message)
+                }
+            })
+        })
+    }
 
-    // const mailOptions: Mail.Options = {
-    //     from: email,
-    //     to: process.env.MY_TIPGMAIL,
-    //     subject: `Message from ${name}`,
-    //     text: `Hello! My name is ${name} and I want to discuss a potential project.\nYou can email me at ${email}.\nHere are some details about my project: ${message}`
-    // }
-
-
-
-
-    // console.log(mailOptions)
-
-    // const sendMailPromise = () => {
-    //     new Promise<string>((resolve, reject) => {
-    //         transport.sendMail(mailOptions, (err) => {
-    //             if(!err){
-    //                 resolve('Email Sent!');
-    //             }else{
-    //                 reject(err.message)
-    //             }
-    //         })
-    //     })
-    // }
-
-    // try {
-    //     await sendMailPromise();
-    //     return NextResponse.json({ message: 'Email Sent!'});
-    // } catch (error) {
-    //     console.error(error);
-    //     return NextResponse.json({ error: error}, { status: 500 })
-    // }
+    try {
+        await sendMailPromise();
+        return NextResponse.json({ message: 'Email Sent!'});
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error}, { status: 500 })
+    }
 }
