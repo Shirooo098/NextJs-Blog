a1266cdff5303cf47a73fbf4ec8ba1fcbfded4b1
Shirooo098 <johndavid8803@gmail.com>
2025-08-01 22:26:13 +0800

    Adjust width size to full

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
       <div className="w-full inline-block py-6">
         <main className="flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]">
         <div className="absolute inset-0 h-full bg-gradient-to-b from-transparent from-0% to-gray-900 rounded-3xl z-0"></div>
-        <div className={`${manRope.className} text-white h-full flex flex-col items-center justify-center z-1`}>
+        <div className={`${manRope.className} text-white size-full flex flex-col items-center justify-center z-1`}>
           <h1 className="w-2/3 sm:w-1/2 text-start sm:text-center font-bold text-5xl xs:text-6xl sxl:text-7xl">Dream Big, Work Hard, Achieve More!</h1>
           <p className="w-2/3 sm:w-1/2  font-medium capitalize mt-5 text-sm sm:text-base">This motto drives my work as an aspiring web developer. I embrace every challenge as it gives me an opportunity to learn. With each project, I aim to achieve long-lasting impact. </p>
         </div>
